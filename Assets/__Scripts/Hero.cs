f3ef694fb25504c3544f2aefdacbcfdbfe6662cb
Shahan Akhter <shahan3.14@gmail.com>
2015-06-30 16:15:09 -0400

    added all enemy prefabs, not sure about hero trigger... and still working on the basic enemy script

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -1,50 +1,108 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour
+{
+    static public Hero S;
 
-	static public Hero		S;
+    public float gameRestartDelay = 2f;
 
-	public float	speed = 30;
-	public float	rollMult = -45;
-	public float  	pitchMult=30;
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
 
-	public float	shieldLevel=1;
+    // Ship status information
+    [SerializeField]
+    private float _shieldLevel = 1; // Add the underscore!
 
-	public bool	_____________________;
-	public Bounds bounds;
+    public bool _____________________;
+    public Bounds bounds;
 
-	void Awake(){
-		S = this;
-		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
-	}
+    void Awake()
+    {
+        S = this;
+        bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+    }
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float xAxis = Input.GetAxis("Horizontal");
-		float yAxis = Input.GetAxis("Vertical");
+    // Use this for initialization
+    void Start()
+    {
 
-		Vector3 pos = transform.position;
-		pos.x += xAxis * speed * Time.deltaTime;
-		pos.y += yAxis * speed * Time.deltaTime;
-		transform.position = pos;
-		
-		bounds.center = transform.position;
-		
-		// constrain to screen
-		Vector3 off = Utils.ScreenBoundsCheck(bounds,BoundsTest.onScreen);
-		if (off != Vector3.zero) {  // we need to move ship back on screen
-			pos -= off;
-			transform.position = pos;
-		}
-		
-		// rotate the ship to make it feel more dynamic
-		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        bounds.center = transform.position;
+
+        // constrain to screen
+        Vector3 off = Utils.ScreenBoundsCheck(bounds, BoundsTest.onScreen);
+        if (off != Vector3.zero)
+        {  // we need to move ship back on screen
+            pos -= off;
+            transform.position = pos;
+        }
+
+        // rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+    // This variable holds a reference to the last triggering GameObject
+    public GameObject lastTriggerGo = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Find the tag of other.gameObject or its parent GameObjects
+        GameObject go = Utils.FindTaggedParent(other.gameObject);
+        // If there is a parent with a tag
+        if (go != null)
+        {
+            // Make sure it's not the same triggering go as last time
+            if (go == lastTriggerGo)
+            { // 2
+                return;
+            }
+            lastTriggerGo = go; // 3
+            if (go.tag == "Enemy")
+            {
+                // If the shield was triggered by an enemy
+                // Decrease the level of the shield by 1
+                shieldLevel--;
+                // Destroy the enemy
+                Destroy(go); // 4
+            }
+            else
+            {
+                print("Triggered: " + go.name); // Move this line here!
+            }
+        }
+    }
+
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel); // 1
+        }
+        set
+        {
+            _shieldLevel = Mathf.Min(value, 4); // 2
+            // If the shield is going to be set to less than zero
+            if (value < 0)
+            {
+                // Tell Main.S to restart the game after a delay
+                Main.S.DelayedRestart(gameRestartDelay);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
 }
